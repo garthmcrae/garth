d9e74ebb3445b32b9310f889ba558dd939ae1f48
Garth <garthmcrae@gmail.com>
2020-01-28 23:07:50 +1100

    Styles, Hamburger location.

--- src/Checkable.js
+++ src/Checkable.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import styles from './checkable.scss';
 
-const Checkable = ({ children, id, name, inputRef, ...props }) => (
-  <>
-    <input className={styles.input} id={id} name={name} ref={inputRef} {...props} />
-    <label className={styles.label} htmlFor={id}>
-      {children}
-    </label>
-  </>
-);
+const Checkable = ({ children, id, inputRef, name, ...props }) => {
+  const { type } = props;
+  console.log(styles[type]);
+  return (
+    <div>
+      <input className={styles.input} id={id} name={name} ref={inputRef} {...props} />
+      <label className={styles.label} htmlFor={id}>
+        {children}
+      </label>
+    </div>
+  );
+};
 
 export default Checkable;
