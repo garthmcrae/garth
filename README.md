b5b918433e3aebc0bf0a458eca3f0f29d30b2e5c
Garth <garthmcrae@gmail.com>
2020-03-01 16:16:55 +1100

    Update vars to use css vars. User select none for clickable elements.

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
-# Components
+# Garth
 
-## Install
+Build responsive, mobile-first projects on the web with the world’s most popular Garth.
+
+## Getting Started
+
+Follow the [create-react-app instructions](https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md).
+
+### Install Garth
 
 ```bash
 npm install --save garth
 ```
 
-## Usage
+### Usage
 
 ```jsx
 import React from 'react';
@@ -15,11 +21,14 @@ import { Button } from 'garth';
 const Example = props => <Button />;
 ```
 
-## Nested example dependencies
+### Dependencies
 
-```js
-    "components": "file:..",
-```
+##### Required Peer Dependencies
+
+These libraries are not bundled with Reactstrap and required at runtime:
+
+  * [**react**](https://www.npmjs.com/package/react)
+  * [**react-dom**](https://www.npmjs.com/package/react-dom)
 
 ## License
 
--- src/index.js
+++ src/index.js
@@ -1,49 +1,23 @@
-import Anchor from './Anchor';
-import Border from './Border';
-import Button from './Button';
-import Box from './Box';
-import Checkable from './Checkable';
-import Container from './Container';
-import Expand from './Expand';
-import Flex from './Flex';
-import Footer from './Footer';
-import Header from './Header';
-import Input from './Input';
-import Label from './Label';
-import List from './List';
-import ListItem from './ListItem';
-import Paragraph from './Paragraph';
-import Root from './Root';
-import Rule from './Rule';
-import Select from './Select';
-import SidebarLayout from './SidebarLayout';
-import SidebarMain from './SidebarMain';
-import SidebarNav from './SidebarNav';
-import Table from './Table';
-import Title from './Title';
-
-export {
-  Anchor,
-  Border,
-  Button,
-  Box,
-  Checkable,
-  Container,
-  Expand,
-  Flex,
-  Footer,
-  Header,
-  Input,
-  Label,
-  List,
-  ListItem,
-  Paragraph,
-  Root,
-  Rule,
-  Select,
-  SidebarLayout,
-  SidebarMain,
-  SidebarNav,
-  Table,
-  Title,
-};
+export { default as Anchor } from './Anchor';
+export { default as Border } from './Border';
+export { default as Button } from './Button';
+export { default as Box } from './Box';
+export { default as Checkable } from './Checkable';
+export { default as Container } from './Container';
+export { default as Expand } from './Expand';
+export { default as Flex } from './Flex';
+export { default as Footer } from './Footer';
+export { default as Header } from './Header';
+export { default as Input } from './Input';
+export { default as Label } from './Label';
+export { default as List } from './List';
+export { default as ListItem } from './ListItem';
+export { default as Paragraph } from './Paragraph';
+export { default as Root } from './Root';
+export { default as Rule } from './Rule';
+export { default as Select } from './Select';
+export { default as SidebarLayout } from './SidebarLayout';
+export { default as SidebarMain } from './SidebarMain';
+export { default as SidebarNav } from './SidebarNav';
+export { default as Table } from './Table';
+export { default as Title } from './Title';
