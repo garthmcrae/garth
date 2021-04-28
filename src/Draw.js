2df54df79e58e2aa3a90aac20f3275b4fdf47ddb
Garth <garthmcrae@gmail.com>
2021-04-29 09:15:06 +1000

    Draw ref fix.

--- src/Draw.js
+++ src/Draw.js
@@ -9,7 +9,7 @@ const Draw = ({ children, controlStyle = {} }) => {
   const [open, setOpen] = useState(false);
   useOnClickOutside(ref, () => setOpen(false));
   return (
-    <div className={`${styles.container} ${open ? styles.active : styles.inactive}`}>
+    <div className={`${styles.container} ${open ? styles.active : styles.inactive}`} ref={ref}>
       <div className={styles.control} style={controlStyle}>
         <Button onClick={_ => setOpen(!open)} type="button">
           <Burger />
