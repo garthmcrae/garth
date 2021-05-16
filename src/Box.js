51a7ce551659ecb0ef1afd16902cf493b39ead04
Garth <garthmcrae@gmail.com>
2021-05-16 12:24:11 +1000

    Box passive attribute.

--- src/Box.js
+++ src/Box.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './box.scss';
 
-const Box = ({ children, element: Element = 'div', fluid, solid, adaptive, width, ...props }) => {
+const Box = ({ adaptive, children, element: Element = 'div', fluid, passive, solid, width, ...props }) => {
   const classList = [styles.box];
   adaptive && classList.push(styles.adaptive);
   fluid && classList.push(styles.fluid);
+  passive ? classList.push(styles.passive) : classList.push(styles.assertive);
   solid && classList.push(styles.solid);
   width && classList.push(styles[`width-${width}`]);
   return (
